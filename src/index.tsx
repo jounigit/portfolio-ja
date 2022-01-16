410fc669d10df2b4ae156a61442e7ecd1b5e66cc
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2022-01-16 19:35:55 +0200

    deploy toshared

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,9 @@ const queryClient = new QueryClient()
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter
+        basename="/test"
+      >
         <App />
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
